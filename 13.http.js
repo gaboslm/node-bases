c064a62547e124a5bcbfd6cb11ffa43dfd3a5c32
Gabriel <gabriel.slmendoza@gmail.com>
2023-08-11 00:20:47 -0500

    server

---
+++ 13.http.js
@@ -0,0 +1,15 @@
+const http = require('node:http')
+const { findAvailablePort } = require('./14.free-port.js')
+
+const desiredPort = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello World\n')
+})
+
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port, () => {
+    console.log(`listening on port http://localhost:${server.address().port}`)
+  })
+})
